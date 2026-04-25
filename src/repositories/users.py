8f5767c6c79ecba09a3f74c9d21334a1ec6a0df3
Alexey Sumarokov <sumarokov.lsha@bk.ru>
2026-04-26 02:38:36 +0300

    add auth

---
+++ src/repositories/users.py
@@ -0,0 +1,19 @@
+from sqlalchemy import select, insert, update, delete
+from pydantic import BaseModel, EmailStr
+
+from src.repositories.base import BaseRepository
+from src.models.users import UserOrm
+from src.schemas.users import User_With_Hashed_Password
+
+
+class UserRepository(BaseRepository):
+    model = UserOrm
+
+    async def get_user_with_hashed_password(self,email:EmailStr):
+        query = select(self.model).filter_by(email=email)
+        result = await self.session.execute(query)
+        model = result.scalars().one()
+        return User_With_Hashed_Password.model_validate(model)
+
+
+
